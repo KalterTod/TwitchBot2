a1b1d0681845d985093082a41d9c20b72fab0f73
Stephen Clark <steve.clark1989@gmail.com>
2021-05-20 00:18:06 -0400

    first commands <3

--- TwitchBot/bot.py
+++ TwitchBot/bot.py
@@ -17,5 +17,13 @@ async def event_ready():
     ws = bot._ws  # this is only needed to send messages within event_ready
     await ws.send_privmsg(os.environ['CHANNEL'], f"/me has landed!")
 
+@bot.command(name='hello')
+async def hello(ctx):
+    await ctx.send(f"Hi, @{ctx.author.name}!")
+
+@bot.command(name='kiss')
+async def kiss(ctx):
+    await ctx.send(f"/me gives @{ctx.author.name} a big old smooch <3")
+
 if __name__ == "__main__":
     bot.run()
